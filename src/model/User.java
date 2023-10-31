2d46437de84ea1bb5f51f96bd10a68b6e4d1b847
SashaBekier <119985211+SashaBekier@users.noreply.github.com>
2023-10-31 11:09:09 +1100

    simple user class and constructor added

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,19 @@
 package model;
 
-public class User {
 
+public class User {
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	public User(String username, String password, String firstName, 
+			String lastName) {
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
 }
